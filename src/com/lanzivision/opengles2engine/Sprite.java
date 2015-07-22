db1cd351564a909cb967d73a5ca847bd3712cc08
John Mikelich <lanzivision@gmail.com>
2015-07-22 16:33:57 -0700

    Update Sprite.java

--- src/com/lanzivision/opengles2engine/Sprite.java
+++ src/com/lanzivision/opengles2engine/Sprite.java
@@ -55,39 +55,40 @@ public class Sprite {
 	 */
 	
 	public Sprite(float positionX, float positionY, int sizeX, int sizeY, Context mContext) {
-		super();
-		me = this;
-		this.positionX = positionX;
-		this.positionY = positionY;
 		
-		//interpolate position with size to compose vertice array
-		this.vertices = new float[]
-		           {
-					positionX - sizeX/2, positionY + sizeY/2, 0.0f,
-					positionX - sizeX/2, positionY - sizeY/2, 0.0f,
-					positionX + sizeX/2, positionY - sizeY/2, 0.0f,
-					positionX + sizeX/2, positionY + sizeY/2, 0.0f,
-		           };
-		// The order of vertexrendering.
-		this.indices = new short[] {0, 1, 2, 0, 2, 3}; 	
+	super();
+	me = this;
+	this.positionX = positionX;
+	this.positionY = positionY;
 		
-		// The vertex buffer.
-		ByteBuffer bb = ByteBuffer.allocateDirect(this.vertices.length * 4);
-		bb.order(ByteOrder.nativeOrder());
-		this.vertexBuffer = bb.asFloatBuffer();
-		this.vertexBuffer.put(this.vertices);
-		this.vertexBuffer.position(0);
+	//interpolate position with size to compose vertice array
+	this.vertices = new float[]
+	{
+		positionX - sizeX/2, positionY + sizeY/2, 0.0f,
+		positionX - sizeX/2, positionY - sizeY/2, 0.0f,
+		positionX + sizeX/2, positionY - sizeY/2, 0.0f,
+		positionX + sizeX/2, positionY + sizeY/2, 0.0f,
+	};
+	// The order of vertexrendering.
+	this.indices = new short[] {0, 1, 2, 0, 2, 3}; 	
+		
+	// The vertex buffer.
+	ByteBuffer bb = ByteBuffer.allocateDirect(this.vertices.length * 4);
+	bb.order(ByteOrder.nativeOrder());
+	this.vertexBuffer = bb.asFloatBuffer();
+	this.vertexBuffer.put(this.vertices);
+	this.vertexBuffer.position(0);
 				
-		// initialize byte buffer for the draw list
-		ByteBuffer dlb = ByteBuffer.allocateDirect(this.indices.length * 2);
-		dlb.order(ByteOrder.nativeOrder());
-		this.drawListBuffer = dlb.asShortBuffer();
-		this.drawListBuffer.put(this.indices);
-		this.drawListBuffer.position(0);
+	// initialize byte buffer for the draw list
+	ByteBuffer dlb = ByteBuffer.allocateDirect(this.indices.length * 2);
+	dlb.order(ByteOrder.nativeOrder());
+	this.drawListBuffer = dlb.asShortBuffer();
+	this.drawListBuffer.put(this.indices);
+	this.drawListBuffer.position(0);
 		
-		//sizes of texture
-		this.sizeX = sizeX;
-		this.sizeY = sizeY;
+	//sizes of texture
+	this.sizeX = sizeX;
+	this.sizeY = sizeY;
 		
 		
 	}
@@ -98,48 +99,48 @@ public class Sprite {
 	public void drawSprite(int mPositionHandle, int mTexCoordLoc, float[] m, Texture currentTexture){
 		
 		
-		 // Set our shader programm
-		GLES20.glUseProgram(ShaderTools.sp_Image);
+	// Set our shader programm
+	GLES20.glUseProgram(ShaderTools.sp_Image);
 	
-		 GLES20.glActiveTexture(GLES20.GL_TEXTURE1);
-	        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, currentTexture.textureIndex);		
-			// Set filtering
-	        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
-	        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
+	GLES20.glActiveTexture(GLES20.GL_TEXTURE1);
+	GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, currentTexture.textureIndex);		
+	// Set filtering
+	GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
+	GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
 	  
-		 // Prepare the triangle coordinate data
-	    GLES20.glVertexAttribPointer(mPositionHandle, 3,
+	// Prepare the triangle coordinate data
+	GLES20.glVertexAttribPointer(mPositionHandle, 3,
 	                                 GLES20.GL_FLOAT, false,
 	                                 0, vertexBuffer);
 	    
-	    // Prepare the texturecoordinates
-	    GLES20.glVertexAttribPointer ( mTexCoordLoc, 2, GLES20.GL_FLOAT,
-                false, 
+	// Prepare the texturecoordinates
+	GLES20.glVertexAttribPointer ( mTexCoordLoc, 2, GLES20.GL_FLOAT,
+        	false, 
                 0, currentTexture.uvBuffer);
 	    
-	    // Get handle to textures locations
+	// Get handle to textures locations
         int mSamplerLoc2 = GLES20.glGetUniformLocation (ShaderTools.sp_Image, "s_texture" );
                 
-		// Set the sampler texture unit to 0, where we have saved the texture.
+	// Set the sampler texture unit to 0, where we have saved the texture.
         GLES20.glUniform1i ( mSamplerLoc2, 1);
 
         // Draw the triangle
         GLES20.glDrawElements(GLES20.GL_TRIANGLES, indices.length,
-                GLES20.GL_UNSIGNED_SHORT, drawListBuffer);
+        GLES20.GL_UNSIGNED_SHORT, drawListBuffer);
         
 
 	}
 	
 	public void resetSprite()
 	{
-		positionX = 0;
-		positionY = 0;
+	positionX = 0;
+	positionY = 0;
 		
-		vertices = new float[0];
-		indices = new short[0];
+	vertices = new float[0];
+	indices = new short[0];
 		
-		sizeX = 0;
-		sizeY = 0;
+	sizeX = 0;
+	sizeY = 0;
 	}
 	
 	/*
@@ -148,21 +149,21 @@ public class Sprite {
 	public void translateSprite(float X, float Y)
 	{
 
-		positionX = X;
-		positionY = Y;
-		this.vertices = new float[]
-		           {
-					positionX - sizeX/2, positionY + sizeY/2, 0.0f,
-					positionX - sizeX/2, positionY - sizeY/2, 0.0f,
-					positionX + sizeX/2, positionY - sizeY/2, 0.0f,
-					positionX + sizeX/2, positionY + sizeY/2, 0.0f,
-			       };
+	positionX = X;
+	positionY = Y;
+	this.vertices = new float[]
+	{
+		positionX - sizeX/2, positionY + sizeY/2, 0.0f,
+		positionX - sizeX/2, positionY - sizeY/2, 0.0f,
+		positionX + sizeX/2, positionY - sizeY/2, 0.0f,
+		positionX + sizeX/2, positionY + sizeY/2, 0.0f,
+	};
 			
-		ByteBuffer bb = ByteBuffer.allocateDirect(vertices.length * 4);
-		bb.order(ByteOrder.nativeOrder());
-		vertexBuffer = bb.asFloatBuffer();
-		vertexBuffer.put(vertices);
-		vertexBuffer.position(0);
+	ByteBuffer bb = ByteBuffer.allocateDirect(vertices.length * 4);
+	bb.order(ByteOrder.nativeOrder());
+	vertexBuffer = bb.asFloatBuffer();
+	vertexBuffer.put(vertices);
+	vertexBuffer.position(0);
 			
 	}
 }
