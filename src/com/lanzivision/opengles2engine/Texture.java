dd3b3725cd15327fea92a0bc8fd8674489fe5c8b
John Mikelich <lanzivision@gmail.com>
2015-07-22 16:34:51 -0700

    Update Texture.java

--- src/com/lanzivision/opengles2engine/Texture.java
+++ src/com/lanzivision/opengles2engine/Texture.java
@@ -24,54 +24,54 @@ public class Texture {
 	public FloatBuffer uvBuffer;
 	
 	public Texture(boolean b, int textureIndexTemp, String textureName, Context mContext) {
-		super();
-		this.isLoaded = b;
 		
-		//this.textureIndex = textureIndexTemp;
+	super();
+	this.isLoaded = b;
+		
+	//this.textureIndex = textureIndexTemp;
 		
 		
-		uvs = new float[] {
-				0.0f, 0.0f,
-				0.0f, 1.0f,
-				1.0f, 1.0f,			
-				1.0f, 0.0f			
-	    };
+	uvs = new float[] {
+		0.0f, 0.0f,
+		0.0f, 1.0f,
+		1.0f, 1.0f,			
+		1.0f, 0.0f			
+	};
 		
-		// The texture buffer
-		ByteBuffer bb = ByteBuffer.allocateDirect(uvs.length * 4);
-		bb.order(ByteOrder.nativeOrder());
-		uvBuffer = bb.asFloatBuffer();
-		uvBuffer.put(uvs);
-		uvBuffer.position(0);
+	// The texture buffer
+	ByteBuffer bb = ByteBuffer.allocateDirect(uvs.length * 4);
+	bb.order(ByteOrder.nativeOrder());
+	uvBuffer = bb.asFloatBuffer();
+	uvBuffer.put(uvs);
+	uvBuffer.position(0);
 				
-		// Check that texture exists
-		int id;
-		File file = new File(textureName);
-		id = mContext.getResources().getIdentifier(textureName, null, mContext.getPackageName());
+	// Check that texture exists
+	int id;
+	File file = new File(textureName);
+	id = mContext.getResources().getIdentifier(textureName, null, mContext.getPackageName());
 			
-		// Temporary create a bitmap
-		Bitmap bmp = BitmapFactory.decodeResource(mContext.getResources(), id);
+	// Temporary create a bitmap
+	Bitmap bmp = BitmapFactory.decodeResource(mContext.getResources(), id);
 		
-		this.textureArray = TextureHelper.loadTextureWithRelease(bmp);
+	this.textureArray = TextureHelper.loadTextureWithRelease(bmp);
         this.textureIndex = textureArray[0];
         
         // We are done using the bitmap so we should recycle it.
-		bmp.recycle();
+	bmp.recycle();
 		
 	}
 	
 	public void resetTexture()
 	{
-		this.isLoaded = false;		
-		this.textureIndex = 0;
+	this.isLoaded = false;		
+	this.textureIndex = 0;
 		
-		//GLES20.glDeleteTextures(1, this.textureIndex);
-		GLES20.glDeleteTextures(1, this.textureArray, 0);
+	GLES20.glDeleteTextures(1, this.textureArray, 0);
 		
-		this.uvs = new float[0];
-		this.textureArray = new int[0];
+	this.uvs = new float[0];
+	this.textureArray = new int[0];
 		
-		//freeNative(this.uvBuffer);
+	//freeNative(this.uvBuffer);
 	}
 
 }
